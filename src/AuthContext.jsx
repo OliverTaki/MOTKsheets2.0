efde2e5f19969fe6c1d7d2d35c86e450542f051a
Daisuke Punch <dicekaydaisuke@gmail.com>
2025-07-09 00:52:53 +0900

    error

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useState, useEffect, useCallback, useRef } from 'react';
 
 export const AuthContext = createContext({});
 const TOKEN_STORAGE_KEY = 'google_auth_token';
