8d5af5f68c19333854956713413d1c88d2113be5
Daisuke Punch <dicekaydaisuke@gmail.com>
2025-07-09 19:11:03 +0900

    check

--- src/utils/google.js
+++ src/utils/google.js
@@ -1,5 +1,11 @@
 export async function fetchGoogle(endpoint, accessToken, params = {}) {
-  const url = new URL(`https://sheets.googleapis.com/v4/${endpoint}`);
+  // drive/ で始まるときは Drive API、そうでなければ Sheets API
+  const base =
+    endpoint.startsWith('drive/')
+      ? 'https://www.googleapis.com/'       // Drive API
+      : 'https://sheets.googleapis.com/v4/'; // Sheets API
+
+  const url = new URL(base + endpoint);
   Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
   const res = await fetch(url.toString(), {
     headers: { Authorization: `Bearer ${accessToken}` },
