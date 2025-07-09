b117bdd53eeddcdfde67a5a4fa62143f2b0ef675
Daisuke Punch <dicekaydaisuke@gmail.com>
2025-07-09 21:55:50 +0900

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 開発モード専用の緩めた CSP（本番 build には入りません）
 const devCsp =
   "default-src 'self'; " +
-  "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com/gsi/client; " +
+  // eval, inline, blob の 3 つを許可
+  "script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://accounts.google.com/gsi/client; " +
   "style-src  'self' 'unsafe-inline' https://accounts.google.com/gsi/style; " +
   "frame-src  'self' https://accounts.google.com/gsi/; " +
-  "connect-src 'self' https://accounts.google.com/gsi/;"
+  "connect-src 'self' https://accounts.google.com/gsi/;";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
